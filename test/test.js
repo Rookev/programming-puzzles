44b696f5d9f02d68506fc42c1ad5e404e4d714a8
Rookev <kevin.roos@web.de>
2020-09-07 19:16:34 +0200

    Additional test cases

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const sumOfNumbers = require('../js/sum-of-numbers/sum-of-numbers.js');
 
-QUnit.module('sumOfNumbers');
+QUnit.module('sum-of-numbers');
 
 QUnit.test('Sum of 1 number', assert => {
   var aTestData = [17];
@@ -8,6 +8,14 @@ QUnit.test('Sum of 1 number', assert => {
   var nActual = sumOfNumbers(aTestData);
 
   assert.equal(nActual, nExpected, 'Sum of 1 number wrong');
+});
+
+QUnit.test('Sum of 2 numbers', assert => {
+  var aTestData = [17,13];
+  var nExpected = 30;
+  var nActual = sumOfNumbers(aTestData);
+
+  assert.equal(nActual, nExpected, 'Sum of 2 numbers wrong');
 });
 
 QUnit.test('Sum of 3 numbers', assert => {
@@ -18,3 +26,34 @@ QUnit.test('Sum of 3 numbers', assert => {
   assert.equal(nActual, nExpected, 'Sum of 3 numbers wrong');
 });
 
+QUnit.test('Sum of no numbers', assert => {
+  var aTestData = [];
+  var nExpected = 0;
+  var nActual = sumOfNumbers(aTestData);
+
+  assert.equal(nActual, nExpected, 'Sum of no numbers wrong');
+});
+
+QUnit.test('Sum of 1 negative number', assert => {
+  var aTestData = [-17];
+  var nExpected = -17;
+  var nActual = sumOfNumbers(aTestData);
+
+  assert.equal(nActual, nExpected, 'Sum of 1 negative number wrong');
+});
+
+QUnit.test('Sum of 2 negative numbers', assert => {
+  var aTestData = [-17, -13];
+  var nExpected = -30;
+  var nActual = sumOfNumbers(aTestData);
+
+  assert.equal(nActual, nExpected, 'Sum of 2 negative numbers wrong');
+});
+
+QUnit.test('Sum of alternating numbers', assert => {
+  var aTestData = [-17, 17];
+  var nExpected = 0;
+  var nActual = sumOfNumbers(aTestData);
+
+  assert.equal(nActual, nExpected, 'Sum of alternating numbers wrong');
+});
