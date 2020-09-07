36b9f88f36e039c85a5da7837712b9cf409519dc
Rookev <kevin.roos@web.de>
2020-09-07 22:13:31 +0200

    Two Sum algorithm

---
+++ js/two-sum.js
@@ -0,0 +1,30 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * 
+ * @param {*} nums Array of integers
+ * @param {*} target Target for indices to add up
+ */
+function twoSum(nums, target) {
+    var resultIndices = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        const firstElement = nums[i];
+
+        // Start second loop on successor of first iterated element (i+1)
+        for (let j = i+1; j < nums.length; j++) {
+            const secondElement = nums[j];
+            
+            if (firstElement + secondElement === target) {
+                resultIndices.push(i);
+                resultIndices.push(j);
+            }
+        }
+        
+    }
+
+    return resultIndices;
+}
+
+module.exports = twoSum;
---
+++ test/two-sum.js
@@ -0,0 +1,39 @@
+const twoSum = require('../js/two-sum.js');
+
+QUnit.module('two-sum');
+
+QUnit.test('Two-Sum of 2 elements array', assert => {
+  var aTestNums = [3,3];
+  var nTestTarget = 6;
+  var aExpectedIndices = [0,1];
+  var nActualIndices = twoSum(aTestNums, nTestTarget);
+
+  assert.deepEqual(nActualIndices, aExpectedIndices, 'Two-Sum of 2 elements array wrong');
+});
+
+QUnit.test('Two-Sum of 3 elements array', assert => {
+  var aTestNums = [3,2,4];
+  var nTestTarget = 6;
+  var aExpectedIndices = [1,2];
+  var nActualIndices = twoSum(aTestNums, nTestTarget);
+
+  assert.deepEqual(nActualIndices, aExpectedIndices, 'Two-Sum of 3 elements array wrong');
+});
+
+QUnit.test('Two-Sum of 4 elements array', assert => {
+  var aTestNums = [2,7,11,15];
+  var nTestTarget = 9;
+  var aExpectedIndices = [0,1];
+  var nActualIndices = twoSum(aTestNums, nTestTarget);
+
+  assert.deepEqual(nActualIndices, aExpectedIndices, 'Two-Sum of 4 elements array wrong');
+});
+
+QUnit.test('Two-Sum of non matching array', assert => {
+  var aTestNums = [1,2,3,4,5];
+  var nTestTarget = 17;
+  var aExpectedIndices = [];
+  var nActualIndices = twoSum(aTestNums, nTestTarget);
+
+  assert.deepEqual(nActualIndices, aExpectedIndices, 'Two-Sum of non matching array wrong');
+});
